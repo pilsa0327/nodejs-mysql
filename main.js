0b32264907c16c9cffa28d5c6be6a0d98d185009
pilsa0327 <52766974+pilsa0327@users.noreply.github.com>
2019-08-14 16:02:37 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var qs = require('querystring');
+var template = require('./lib/template.js');
+var path = require('path');
+var mysql= require('mysql');
+var db = require('./lib/db');
+var topic = require('./lib/topic');
+var author = require('./lib/author');
+
+var app = http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
+    if(pathname === '/'){
+      if(queryData.id === undefined){
+        topic.home(request, response);
+      } else {
+        topic.page(request, response);
+      }
+    } else if(pathname === '/create'){
+      topic.create(request, response);  
+    } else if(pathname === '/create_process'){
+      topic.create_process(request, response);
+    } else if(pathname === '/update'){
+      topic.update(request, response);
+    } else if(pathname === '/update_process'){
+      topic.update_process(request, response);
+    } else if(pathname === '/delete_process'){
+      topic.delete_process(request, response);
+    } else if(pathname === '/author'){
+      author.index(request, response);
+    } else if(pathname === '/author/create_process'){
+      author.author_create_process(request, response);
+    } else if(pathname === '/author/update'){
+      author.author_update(request, response); 
+    } else if(pathname === '/author/update_process'){
+      author.author_update_process(request, response);
+    } else if(pathname === '/author/delete_process'){
+      author.author_delete_process(request, response);   
+    } else {
+      response.writeHead(404);
+      response.end('Not found');
+    }
+});
+app.listen(3000);
